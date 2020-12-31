9bfd6ccee606276933877a11ae2df454313f6f3d
MaxKlenin <72246785+MaxKlenin@users.noreply.github.com>
2020-12-31 17:53:32 +0200

    Add files via upload

---
+++ PatternSingleton/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using PatternStrategy.DuckType;
+using System.Collections.Generic;
+using System;
+
+namespace PatternSingleton
+{
+    public sealed class Singleton
+    {
+        private static Singleton _instance;
+        private static Dictionary<BaseDuck, int> ducksDict = new Dictionary<BaseDuck, int>(3);
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton getInstance()
+        {
+            if (_instance == null)
+                _instance = new Singleton();
+            return _instance;
+        }
+
+        public void PackDuck(BaseDuck baseDuck, int count)
+        {   
+            Console.Write($"On factory was packed:{count} - ");
+            baseDuck.ShowName();
+            try
+            {
+                ducksDict.Add(baseDuck, count);
+            }
+            catch (ArgumentException)
+            {
+                ducksDict[baseDuck] += count;
+            }
+        }
+    }
+}
